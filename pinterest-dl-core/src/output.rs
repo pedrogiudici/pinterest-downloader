b01ae267e92a117c5683fa994d4540dff23ccaff
Pedro Giudici <q0u9rfp7@shusud.anonaddy.me>
2026-07-04 18:20:52 -0300

    refactor: replace push_event helper with inline emit closure

--- pinterest-dl-core/src/output.rs
+++ pinterest-dl-core/src/output.rs
@@ -33,58 +33,34 @@ pub struct DownloadWorker {
     events: Arc<Mutex<Vec<DownloadEvent>>>,
 }
 
-impl DownloadWorker {
-    fn push_event(events: &Arc<Mutex<Vec<DownloadEvent>>>, event: DownloadEvent) {
-        if let Ok(mut events) = events.lock() {
-            events.push(event);
-        }
-    }
-}
-
 impl Output for DownloadWorker {
     fn handle_submission(&mut self, pin_url: &str, directory_path: &str, id: DownloadId) {
         let pin_url = pin_url.to_owned();
         let directory_path = directory_path.to_owned();
         let events = Arc::clone(&self.events);
 
-        std::thread::spawn(move || match extract_download_url(&pin_url) {
-            Ok(download_url) => {
-                let filename =
-                    filename_from_url(&download_url).unwrap_or_else(|| "video.mp4".to_owned());
-                Self::push_event(
-                    &events,
-                    DownloadEvent {
-                        id,
-                        kind: DownloadEventKind::FileNameResolved(filename.clone()),
-                    },
-                );
-
-                let dest = PathBuf::from(&directory_path).join(&filename);
-
-                match download_video(&download_url, &dest) {
-                    Ok(()) => Self::push_event(
-                        &events,
-                        DownloadEvent {
-                            id,
-                            kind: DownloadEventKind::Completed,
-                        },
-                    ),
-                    Err(e) => Self::push_event(
-                        &events,
-                        DownloadEvent {
-                            id,
-                            kind: DownloadEventKind::Failed(format!("{e:?}")),
-                        },
-                    ),
+        std::thread::spawn(move || {
+            let emit = |kind: DownloadEventKind| {
+                if let Ok(mut events) = events.lock() {
+                    events.push(DownloadEvent { id, kind });
                 }
+            };
+
+            match extract_download_url(&pin_url) {
+                Ok(download_url) => {
+                    let filename =
+                        filename_from_url(&download_url).unwrap_or_else(|| "video.mp4".to_owned());
+                    emit(DownloadEventKind::FileNameResolved(filename.clone()));
+
+                    let dest = PathBuf::from(&directory_path).join(&filename);
+
+                    match download_video(&download_url, &dest) {
+                        Ok(()) => emit(DownloadEventKind::Completed),
+                        Err(e) => emit(DownloadEventKind::Failed(format!("{e:?}"))),
+                    }
+                }
+                Err(e) => emit(DownloadEventKind::Failed(format!("{e:?}"))),
             }
-            Err(e) => Self::push_event(
-                &events,
-                DownloadEvent {
-                    id,
-                    kind: DownloadEventKind::Failed(format!("{e:?}")),
-                },
-            ),
         });
     }
 
